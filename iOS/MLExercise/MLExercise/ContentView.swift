6352db6a76badfed5c00e0320488a9150d1310c7
Nick Wang <wwwhackcom@qq.com>
2024-04-11 09:37:14 +1200

    ios init

---
+++ iOS/MLExercise/MLExercise/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MLExercise
+//
+//  Created by Nick Wang on 10/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS/MLExercise/MLExercise/MLExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLExerciseApp.swift
+//  MLExercise
+//
+//  Created by Nick Wang on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MLExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
