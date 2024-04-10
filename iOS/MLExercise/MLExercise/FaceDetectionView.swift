7fc2061d976c7b16fbfefd08b398c612922c6a05
Nick Wang <wwwhackcom@qq.com>
2024-04-11 09:44:03 +1200

    added asset images

---
+++ iOS/MLExercise/MLExercise/FaceDetectionView.swift
@@ -0,0 +1,26 @@
+//
+//  FaceDetectionView.swift
+//  MLExercise
+//
+//  Created by Nick Wang on 11/04/24.
+//
+
+import SwiftUI
+
+struct FaceDetectionView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct FaceDetectionView_Previews: PreviewProvider {
+    static var previews: some View {
+        FaceDetectionView()
+    }
+}
