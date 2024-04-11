e8cf3b97be7c9532755db7c03d205812c9a18718
Nick Wang <wwwhackcom@qq.com>
2024-04-11 21:26:24 +1200

    added UI elements

--- iOS/MLExercise/MLExercise/FaceDetectionView.swift
+++ iOS/MLExercise/MLExercise/FaceDetectionView.swift
@@ -8,16 +8,58 @@
 import SwiftUI
 
 struct FaceDetectionView: View {
+    @State private var index = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let uiImage = UIImage(named: "FDImages/face_test_\(index).jpg") {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: UIScreen.main.bounds.width)
+            }
+            HStack {
+                Button(action: {
+                    index = getIndex(index - 1)
+                }, label: {
+                    Image(systemName: "arrow.left")
+                        .font(.system(size: 30))
+                        .tint(.secondary)
+                })
+
+                Button(action: {
+                    
+                }) {
+                    Text("Decode")
+                        .font(.title)
+                        .padding(20)
+                        .background(.gray)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                Button(action: {
+                    index = getIndex(index + 1)
+                }, label: {
+                    Image(systemName: "arrow.right")
+                        .font(.system(size: 30))
+                        .tint(.secondary)
+                })
+            }
+            .padding()
         }
         .padding()
     }
 }
+
+private func getIndex(_ index: Int, minIndex: Int = 1, maxIndex: Int = 5) -> Int {
+    if index < minIndex {
+        return maxIndex
+    } else if index > maxIndex {
+        return minIndex
+    } else {
+        return index
+    }
+}
+
 
 struct FaceDetectionView_Previews: PreviewProvider {
     static var previews: some View {
